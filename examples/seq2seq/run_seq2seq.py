6c20f24b7c971431307d392b9f4193f19a084d8c
SteveMama <pkompally@gmail.com>
2021-03-10 22:45:38 +0530

    Update run_seq2seq.py

--- examples/seq2seq/run_seq2seq.py
+++ examples/seq2seq/run_seq2seq.py
@@ -602,6 +602,8 @@ def main():
         trainer.save_model("/content/pegasus-samsum")
         tokenizer.save_vocabulary("/content/pegasus-samsum")
         tokenizer.save_pretrained("/content/pegasus-samsum")
+        trainer.save_model("/content/drive/MyDrive/pegasus-samsum")
+        tokenizer.save_pretrained("/content/drive/MyDrive/pegasus-samsum")
         
    
 
